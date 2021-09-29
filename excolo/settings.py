2e9270f6b684ff88ba051b43e0bd382b1066cb04
Hantsi <hantsitaumang@gmail.com>
2021-09-29 15:55:59 +0200

    Debug

--- excolo/settings.py
+++ excolo/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7x21!pq!8qf!fc#mn#k@s)kp%89fo5_n6w%ie(8)xgsnnz145z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
